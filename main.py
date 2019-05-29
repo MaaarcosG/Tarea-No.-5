0aebca6c870d3b7b61668522a045e3f9e6e5c7a7
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-05-29 08:22:49 -0600

    Se reviso y se cambio el modelo se prueba para demostración

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ from bitmap import *
 Por fines de puebra por alguna razon con el modelo de ReyBoo.mtl y ReyBoo.obj los colores no se añaden
 por lo cual para pobrar el codigo se utilizo otro modelo
 '''
-def reyBoo():
+def batman():
 	renderizando = Bitmap(600,600)
 	#renderizando.renderer(./modelos/test3.obj, scale=(0,0,0), translate=(0,0,0))
-	renderizando.renderer_color('./modelos/batman.obj', mtl='./modelos/batman.mtl', scale=(100,100,100), translate=(3,3,3))
+	renderizando.renderer_color('./modelos/p3.obj', mtl='./modelos/p3.mtl', scale=(100,100,100), translate=(3,3,3))
 	renderizando.archivo('output.bmp')
+
+def lego():
+	renderizando = Bitmap(600,600)
+	#renderizando.renderer(./modelos/test3.obj, scale=(0,0,0), translate=(0,0,0))
+	renderizando.renderer_color('./modelos/p3.obj', mtl='./modelos/p3.mtl', scale=(100,100,100), translate=(3,3,3))
+	renderizando.archivo('lego.bmp')
 
 print("Renderizando los modelos obj")
 
 print("Renderizando Modelo de blender")
-print(reyBoo())
+print(lego())
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Por demostración se cambio el modelo principal el cual era batman de prueba, debido a que tiene
+color negro de fondo y batman la mayoria se encuntra de negro. El modelo al cual se cambio es un lego
+el cual si tiene colores.
+
+# EJECUCIÓN
+Para ejecutar y revisar el modelo unicamente tiene que correr el archivo main.py 
